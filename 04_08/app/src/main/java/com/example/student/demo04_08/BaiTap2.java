d98342945f3b5726f89228938bada0ca2cd4c73b
ToanTruong <Toan2671997@gmail.co>
2020-08-04 17:19:43 +0700

    04/08

---
+++ 04_08/app/src/main/java/com/example/student/demo04_08/BaiTap2.java
@@ -0,0 +1,13 @@
+package com.example.student.demo04_08;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaiTap2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai_tap2);
+    }
+}
---
+++ 04_08/baitap4-2/src/main/java/com/example/baitap4_2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.baitap4_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
